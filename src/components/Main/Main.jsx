bb197e886c78009f0846694fb9d0291528767830
smartboy0310 <qdustudent6@gmail.com>
2022-04-07 12:43:18 +0500

    Add game section

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import './Main.css';
 
 import Slider from '../HomeSlider/Slider';
+import gameOne from '../../Assets/Images/game_one.png';
+import gameTwo from '../../Assets/Images/game_two.png';
+import gameThree from '../../Assets/Images/game_three.png';
 
 function Main() {
 	return (
@@ -9,22 +12,61 @@ function Main() {
 			<main className="main">
 				<section className="top-shrine">
 					<div className="container">
-					<div className="main__top--shrines">
-						<h2 className="main-heading">Top Ziyoratgohlar</h2>
-						<p className="main-description">
-							O’zbekistonning shonli tarixi haqida siz bilgan va
-							bilmagan faktlar
-						</p>
-					</div>
+						<div className="main__top--shrines">
+							<h2 className="main-heading">Top Ziyoratgohlar</h2>
+							<p className="main-description">
+								O’zbekistonning shonli tarixi haqida siz bilgan
+								va bilmagan faktlar
+							</p>
+						</div>
 					</div>
 					<Slider />
 				</section>
 				<section className="flok-game">
-					<h2 className="game-heading">Mashxur xalq o’yinlari</h2>
-					<p className="game-description">
-						O’zbekistonning xalq orasida mashxur bo’lgan milliy
-						o’yinlari.
-					</p>
+					<div className="container">
+						<h2 className="game-heading">Mashxur xalq o’yinlari</h2>
+						<p className="game-description">
+							O’zbekistonning xalq orasida mashxur bo’lgan milliy
+							o’yinlari.
+						</p>
+						<div className="game-box">
+							<img
+							className='game-one'
+								src={gameOne}
+								alt="Kupkari game"
+								width={451}
+								height={546}
+							/>
+							<div className="game-info">
+								<div className="game-img-box">
+									<img
+									className='game-two'
+										src={gameTwo}
+										alt="Chillak game"
+										width={320}
+										height={260}
+									/>
+									<img
+										src={gameThree}
+										alt="Tug of war game"
+										width={320}
+										height={260}
+									/>
+								</div>
+								<p className="game-discription">
+									Mamlakatimiz mustaqillikka erishgan
+									dastlabki kunlardan boshlab, milliy
+									qadriyatlarimiz, an'ana-yu udumlarimiz, shu
+									jumladan, xalq o‘yinlarini qayta tiklashga
+									katta e'tibor qaratildi. Sportning an'anaviy
+									turlari bilan birgalikda milliy sport
+									turlari hamda xalq o‘yinlari ham tobora
+									rivojlanib borayotir
+								</p>
+								<button className='game-more-btn'>batafsil</button>
+							</div>
+						</div>
+					</div>
 				</section>
 			</main>
 		</>
